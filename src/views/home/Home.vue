bfa45ba2b4b0da55f0b3b8fc7d34ca68fba2c903
aikang <1347371844@qq.com>
2024-07-05 18:29:14 +0800

    test

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,15 +5,29 @@
 
 <script setup>
 // 设置token
-import * as cesium from "cesium"
+import * as Cesium from "cesium"
 import {onMounted} from "vue";
 
-cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxOTU2ZWQyNy1jMmMzLTQwZTMtOTI1My0yODk1NTY5ZGIzMjkiLCJpZCI6OTQyNDksImlhdCI6MTcyMDE2NTQyMX0.XuU0b2FLYis2zt4HH3aNyV8LXuy7Ju6rBPgwWfz4z8Y"
+let esri = new Cesium.ArcGisMapServerImageryProvider({
+  url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
+});
+Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxOTU2ZWQyNy1jMmMzLTQwZTMtOTI1My0yODk1NTY5ZGIzMjkiLCJpZCI6OTQyNDksImlhdCI6MTcyMDE2NTQyMX0.XuU0b2FLYis2zt4HH3aNyV8LXuy7Ju6rBPgwWfz4z8Y"
 // 因为加载时div还未挂在好所以使用生命周期处理
 onMounted(() => {
-  new cesium.Viewer("cesiumMap", {
-    infoBox: false
+  let cesiumMap = new Cesium.Viewer("cesiumMap", {
+    infoBox: false,
+    // 关闭自带地图选择
+    baseLayerPicker: false,
+    // 使用的底图
+    // imageryProvider: esri,
+    // 设置地形
+    terrain: Cesium.Terrain.fromWorldTerrain(),
+  });
+  // 设置相机位置
+  cesiumMap.camera.flyTo({
+    destination:{}
   })
+
 })
 
 </script>
